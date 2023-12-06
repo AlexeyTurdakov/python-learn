7465c86049a1c96f9e4dad8ef5aa1ab64155627c
Alexey Turdakov <alexeyturdakov@gmail.con>
2023-12-06 22:27:45 +0400

    lesson3

--- lesson2.py
+++ lesson2.py
@@ -4,4 +4,23 @@ name = 'irbis'
 
 names = ['1', 1, name]
 
-print(names[2].title())
+print(f'{names[2].title()} {names[-3]}')
+
+names[1] = 2
+
+names.append(10)
+names.append('100')
+names.insert(1, '1000')
+del names[0]
+pop_method = names.pop(2)
+names.remove(2)
+names.remove('100')
+print(names)
+print(pop_method)
+
+gosts = ['egy', 'rog']
+
+deleteGosts = gosts.pop(0)
+print(gosts, deleteGosts)
+
+
